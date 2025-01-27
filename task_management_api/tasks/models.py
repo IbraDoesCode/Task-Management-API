302ceba4847a04bdb66b4783e8cbd7a24ffb113a
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-01-27 21:05:38 +0800

    Remove unnecessary comments

--- task_management_api/tasks/models.py
+++ task_management_api/tasks/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Task (models.Model):
-    title = models.CharField(max_length=255)  # Task title with a max length
-    description = models.TextField()  # Optional detailed task description
-    assigned_to = models.ForeignKey(User, related_name='tasks', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set creation timestamp
-    is_completed = models.BooleanField(default=False)  # Task completion status
-    updated_at = models.DateTimeField(auto_now=True)  # Automatically set update timestamp
+    title = models.CharField(max_length=255)  
+    description = models.TextField()  
+    assigned_to = models.ForeignKey(User, related_name='tasks', on_delete=models.CASCADE) # Here we are referencing to Django's built-in User model
+    created_at = models.DateTimeField(auto_now_add=True)  
+    is_completed = models.BooleanField(default=False)  
+    updated_at = models.DateTimeField(auto_now=True) 
 
     def __str__(self):
         return f'Task: {self.title} - Assigned to: {self.assigned_to.username} status: {self.is_completed}'
